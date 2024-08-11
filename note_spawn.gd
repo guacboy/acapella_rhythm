1dc23a50550b809954731d05a762f34b06d72bad
Dylan <rattledew17@gmail.com>
2024-08-11 13:59:39 -0700

    fixed notes not disappearing after input

--- note_spawn.gd
+++ note_spawn.gd
@@ -4,12 +4,15 @@ extends Marker2D
 
 func _ready() -> void:
 	connect_note_to_key("s-key")
-	connect_note_to_key("j-key")
-		
-func connect_note_to_key(note: String) -> void:
-	if note_connected_to == note:
-		spawn_note()
+	connect_note_to_key("f-key")
 
-func spawn_note() -> void:
+# if the note_key matches with the note_connected_to, the note will spawn on that respective lane
+# ex. note_key = "s-key" means it will spawn where the s-key column is (aka, the first lane)
+func connect_note_to_key(note_key: String) -> void:
+	if note_connected_to == note_key:
+		spawn_note(note_key)
+
+func spawn_note(note_key: String) -> void:
 	var note_instance := preload("res://Common/Interface/Beats/note.tscn").instantiate()
+	note_instance.key = note_key
 	add_child(note_instance)
