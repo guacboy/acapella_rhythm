6e91f61866d8819572ec135b70766bb00fda9563
Dylan <rattledew17@gmail.com>
2024-08-11 18:19:52 -0700

    added adjustable tempo between notes

--- Music/Copyright/siinamota - Young Girl A/siinamota-young_girl_a.gd
+++ Music/Copyright/siinamota - Young Girl A/siinamota-young_girl_a.gd
@@ -13,15 +13,15 @@ var note_list := {
 	}
 }
 
+var note_list_idx = note_list[1] # allows for jumping between different parts of the song
+
 func _ready() -> void:
 	#track.play()
 	
-	var note_list_idx = note_list[1] # allows for jumping between different parts of the song
-	Signals.emit_signal("change_to_new_speed", note_list_idx["tempo"])
 	play_note(note_list_idx["note"])
-	print(note_list_idx["tempo"])
 
 func play_note(note) -> void:
 	for i in note:
 		Signals.emit_signal("spawn_note", i)
+		Signals.emit_signal("change_to_new_speed", note_list_idx["tempo"])
 		await get_tree().create_timer(0.5).timeout
