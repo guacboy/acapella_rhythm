b227aaf1ed93e124cc0f5411436a229cb6183541
Dylan <rattledew17@gmail.com>
2024-08-18 14:27:57 -0700

    added despawn barrier
    added long notes fading and shortening mechanics

--- Common/Interface/Gameplay/long_note.gd
+++ Common/Interface/Gameplay/long_note.gd
@@ -5,10 +5,12 @@ extends Node2D
 @onready var sprite_2d = $Sprite2D
 @onready var body_area = $BodyArea
 @onready var line = $Line
+@onready var line_area = $LineArea
 
 var is_on_button: bool = false # within "miss" range
 var is_exactly_on_button: bool = false # within "okay" range
 var is_key_held_down: bool = false
+var is_out_of_bounds: bool = false
 var note_speed: int = 0
 
 func _ready() -> void:
@@ -17,28 +19,49 @@ func _ready() -> void:
 func _physics_process(delta) -> void:
 	position.y += note_speed
 	
+	# shrinks the line as the key is being pressed
 	if is_key_held_down and line.scale.x >= 0.0:
-		line.scale.x -= 0.2
-		line.position.y -= 1.0
+		shorten_line()
 	if is_key_held_down and not is_exactly_on_button and line.scale.x >= 0.0:
-		line.scale.x -= 0.2
-		line.position.y -= 1.0
+		shorten_line()
+	if is_out_of_bounds and not is_key_held_down and line.scale.x >= 0.0:
+		shorten_line()
+		fade_out(sprite_2d)
+		fade_out(line)
 
 func _input(event) -> void:
+	# if miss, disables the line
 	if event.is_action_pressed(key) and is_on_button and not is_exactly_on_button:
-		queue_free()
+		body_area.queue_free()
+		line_area.queue_free()
+		fade_out(sprite_2d)
+		fade_out(line)
 	
 	# gets rid of the note head
 	if event.is_action_pressed(key) and is_exactly_on_button:
 		is_key_held_down = true
-		line.modulate.a = 0.25
+		fade_out(line)
 		sprite_2d.queue_free()
 		body_area.queue_free()
+		
 	# breaks combo and disables the line
 	if event.is_action_released(key) and is_exactly_on_button:
 		is_key_held_down = false
 		Signals.emit_signal("on_combo_increment", false)
 		key = ""
+
+# shortens the line sprite
+func shorten_line() -> void:
+	line.scale.x -= 0.2
+	line.position.y -= 1.55
+
+# fades out the note if out of bounds
+func fade_out(object: Sprite2D) -> void:
+	object.modulate.a = 0.25
+	
+	var tween := create_tween()
+	tween.tween_property(object, "modulate:a", 0.0, 0.3)
+	tween.finished.connect(queue_free)
 
 # changes how fast the notes will come down, allowing for adjustable tempos
 func _change_to_new_speed(speed: int) -> void:
@@ -55,6 +78,8 @@ func _on_area_2d_area_exited(area):
 func _on_line_area_area_entered(area):
 	if area.is_in_group("okay"):
 		is_exactly_on_button = true
+	if area.is_in_group("despawn"):
+		is_out_of_bounds = true
 
 func _on_line_area_area_exited(area):
 	if area.is_in_group("okay"):
